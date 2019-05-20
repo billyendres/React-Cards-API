5dc0e52e231aace71546c708c8ec382d6bcbb141
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-05-20 10:17:24 +1000

    Updates

---
+++ precache-manifest.6dab1a8ece41ceef4568d053d253e1a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de628c0f45b4cbc0ce1e6cb6a37c6a0d",
+    "url": "/react-cards-api/index.html"
+  },
+  {
+    "revision": "2a1d4e501c12481fc0b9",
+    "url": "/react-cards-api/static/css/main.605ebaa2.chunk.css"
+  },
+  {
+    "revision": "dcb5e5a2b063bc8f7387",
+    "url": "/react-cards-api/static/js/2.269d2553.chunk.js"
+  },
+  {
+    "revision": "2a1d4e501c12481fc0b9",
+    "url": "/react-cards-api/static/js/main.03798a6f.chunk.js"
+  },
+  {
+    "revision": "d72b5da3a7b69bbcc00a",
+    "url": "/react-cards-api/static/js/runtime~main.a92911af.js"
+  }
+]);
